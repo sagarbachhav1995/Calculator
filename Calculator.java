35541c8073a25db9b73f958180b0f6f21e902ca1
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-02 19:04:50 +0530

    Addition

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package com.blz.java;
+//import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a = 7;
+    int b = 5;
+    int sum = a+b;
+    System.out.println("Sum of: "+sum);
+	}
+
+}
